a91cefd8f39406cd478469d870e4c1697cf36874
Norbert Śleziak <norr.bitt@gmail.com>
2024-01-05 18:15:19 +0100

    Update FileController.java

--- backend/src/main/java/pl/norbit/filetranslator/controller/FileController.java
+++ backend/src/main/java/pl/norbit/filetranslator/controller/FileController.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.norbit.filetranslator.model.TranslateInfo;
 import pl.norbit.filetranslator.service.FileService;
@@ -16,6 +13,7 @@ import pl.norbit.filetranslator.service.FileService;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@CrossOrigin
 public class FileController {
 
     private final FileService fileService;
